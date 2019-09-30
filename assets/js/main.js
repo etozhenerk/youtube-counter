6bfe7fc9726c0090743a8062d4d3d67bd7f31331
etozhenerk <etozhenerk@yandex.ru>
2019-09-30 16:07:21 +0300

    Асинхроный запрос, вывод общего числа подписчиков и описания канала в консоль

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,46 @@
 "use strict";
 
-var xhr = new XMLHttpRequest();
+var HttpClient = function HttpClient() {
+  this.get = function (aUrl, aCallback) {
+    var anHttpRequest = new XMLHttpRequest();
+    anHttpRequest.onreadystatechange = function () {
+      if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200) aCallback(anHttpRequest.responseText);
+    };
 
-xhr.open("GET", "https://www.googleapis.com/youtube/v3/channels?part=statistics&id=UCVswRUcKC-M35RzgPRv8qUg&key=AIzaSyB7pT0HMSiGxTzmU8gI5kR3XRTAbqt55k0", false);
-xhr.send();
+    anHttpRequest.open("GET", aUrl, true);
+    anHttpRequest.send(null);
+  };
+};
 
-if (xhr.status != 200) {
-  console.log(xhr.status + ":" + xhr.statusText);
-} else {
-  var subsCount = JSON.parse(xhr.responseText).items[0].statistics.subscriberCount;
-  document.querySelector("#count").textContent = subsCount;
-}
+var client = new HttpClient();
+
+var showStat = function showStat() {
+  client.get("https://www.googleapis.com/youtube/v3/channels?part=statistics&id=UCVswRUcKC-M35RzgPRv8qUg&key=AIzaSyB7pT0HMSiGxTzmU8gI5kR3XRTAbqt55k0", function (response) {
+    console.log(JSON.parse(response).items[0].statistics.subscriberCount);
+  });
+};
+var showDescr = function showDescr() {
+  client.get("https://www.googleapis.com/youtube/v3/channels?part=snippet&id=UCVswRUcKC-M35RzgPRv8qUg&key=AIzaSyB7pT0HMSiGxTzmU8gI5kR3XRTAbqt55k0", function (response) {
+    console.log(JSON.parse(response).items[0].snippet.description);
+  });
+};
+
+showStat();
+showDescr();
+
+// let xhr = new XMLHttpRequest();
+
+// xhr.open(
+//   "GET",
+//   "https://www.googleapis.com/youtube/v3/channels?part=statistics&id=UCVswRUcKC-M35RzgPRv8qUg&key=AIzaSyB7pT0HMSiGxTzmU8gI5kR3XRTAbqt55k0",
+//   false
+// );
+// xhr.send();
+
+// if (xhr.status != 200) {
+//   console.log(xhr.status + ":" + xhr.statusText);
+// } else {
+//   let subsCount = JSON.parse(xhr.responseText).items[0].statistics
+//     .subscriberCount;
+//   document.querySelector("#count").textContent = subsCount;
+// }
--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,46 @@
-let xhr = new XMLHttpRequest();
+var HttpClient = function() {
+  this.get = function(aUrl, aCallback) {
+    let anHttpRequest = new XMLHttpRequest();
+    anHttpRequest.onreadystatechange = function() {
+      if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)
+        aCallback(anHttpRequest.responseText);
+    };
 
-xhr.open(
-  "GET",
-  "https://www.googleapis.com/youtube/v3/channels?part=statistics&id=UCVswRUcKC-M35RzgPRv8qUg&key=AIzaSyB7pT0HMSiGxTzmU8gI5kR3XRTAbqt55k0",
-  false
-);
-xhr.send();
+    anHttpRequest.open("GET", aUrl, true);
+    anHttpRequest.send(null);
+  };
+};
 
-if (xhr.status != 200) {
-  console.log(xhr.status + ":" + xhr.statusText);
-} else {
-  let subsCount = JSON.parse(xhr.responseText).items[0].statistics.subscriberCount;
-  document.querySelector("#count").textContent = subsCount;
-}
+let client = new HttpClient();
+
+let showStat = () => {
+  client.get("https://www.googleapis.com/youtube/v3/channels?part=statistics&id=UCVswRUcKC-M35RzgPRv8qUg&key=AIzaSyB7pT0HMSiGxTzmU8gI5kR3XRTAbqt55k0",(response) => {
+    console.log(JSON.parse(response).items[0].statistics
+    .subscriberCount);
+  });
+};
+let showDescr = () => {
+  client.get("https://www.googleapis.com/youtube/v3/channels?part=snippet&id=UCVswRUcKC-M35RzgPRv8qUg&key=AIzaSyB7pT0HMSiGxTzmU8gI5kR3XRTAbqt55k0",(response) => {
+    console.log(JSON.parse(response).items[0].snippet.description);
+  });
+};
+
+showStat();
+showDescr();
+
+// let xhr = new XMLHttpRequest();
+
+// xhr.open(
+//   "GET",
+//   "https://www.googleapis.com/youtube/v3/channels?part=statistics&id=UCVswRUcKC-M35RzgPRv8qUg&key=AIzaSyB7pT0HMSiGxTzmU8gI5kR3XRTAbqt55k0",
+//   false
+// );
+// xhr.send();
+
+// if (xhr.status != 200) {
+//   console.log(xhr.status + ":" + xhr.statusText);
+// } else {
+//   let subsCount = JSON.parse(xhr.responseText).items[0].statistics
+//     .subscriberCount;
+//   document.querySelector("#count").textContent = subsCount;
+// }
