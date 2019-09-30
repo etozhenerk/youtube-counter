40880441daaf1a050c649c714da4b6294fe7b963
etozhenerk <etozhenerk@yandex.ru>
2019-09-30 15:45:32 +0300

    Первый запрос на сервер

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,13 @@
 "use strict";
+
+var xhr = new XMLHttpRequest();
+
+xhr.open("GET", "https://www.googleapis.com/youtube/v3/channels?part=statistics&id=UCVswRUcKC-M35RzgPRv8qUg&key=AIzaSyB7pT0HMSiGxTzmU8gI5kR3XRTAbqt55k0", false);
+xhr.send();
+
+if (xhr.status != 200) {
+  console.log(xhr.status + ":" + xhr.statusText);
+} else {
+  var subsCount = JSON.parse(xhr.responseText).items[0].statistics.subscriberCount;
+  document.querySelector("#count").textContent = subsCount;
+}
